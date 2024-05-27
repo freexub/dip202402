33942346de63aa09b66f032bd0fce09fd3ba7d5d
freexub <freexub@gmail.com>
2024-05-27 19:49:32 +0500

    Second commit

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'name' => 'ПЕРСОНАЛ',
     'language' => 'Ru-ru',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
@@ -81,7 +82,7 @@ $config = [
         'class' => 'mdm\admin\components\AccessControl',
         'allowActions' => [
             'site/*',
-            'personal/*',
+//            'personal/*',
 //            'rbac/*',
         ]
     ],
--- modules/personal/views/layouts/sidebar.php
+++ modules/personal/views/layouts/sidebar.php
@@ -1,5 +1,5 @@
 <aside class="main-sidebar sidebar-light-green elevation-4">
-    <a href="index3.html" class="brand-link">
+    <a href="/site/index" class="brand-link">
         <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">ПЕРСОНАЛ</span>
     </a>
